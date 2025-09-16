03d022725e32520378e2d13bdbf3a992cdc7dc6b
jeffschoe <ratacame@protonmail.com>
2025-09-16 17:00:57 -0400

    Setup .venv, added notes in the README, and created basic startup process.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # octopus-lte-signal-meter-report-analyzer
 CLI based tool for accepting CSV signal meter reports and giving an analysis/recommendation based on the data.
+
+# Requirements 
+uv
+pandas
+
+# usage
+Add cellular signal reports in the inputs/ directory
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-
+import sys
 
 
 def main():
-    pass
-
+    verbose = "--verbose" in sys.argv
+    if not sys.argv[1:]:
+        print("Octpopus LTE Signal Meter Report Analyzer")
+        print('\nAnalyzes any reports in the reports/ directory')
+        print('Usage: python main.py [--verbose]')
+        print('[--verbose] prints results to the console')
+        print('Results are stored in the results/ directory\n')
+        sys.exit(1)
 
 
 
